1dc4d7c305d9ddfdda0a7468f33421366984741e
ariframaps <ariframa@student.ub.ac.id>
2024-04-09 19:46:44 +0700

    styling productCard

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,5 +1,21 @@
+import { ProductCard } from "../components/ProductCard"
+
 export const ProductList = ({ apiPath }) => {
     return (
-        <main>ProductsList: {apiPath}</main>
+        <main>
+            ProductsList: {apiPath}
+            <div className="flex justify-center items-center">
+                <div className="max-w-screen-xl grid grid-cols-3 gap-10 bg-red-200">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
+            </div>
+        </main>
     )
 }
