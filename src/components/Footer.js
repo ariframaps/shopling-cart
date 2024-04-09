a9b3668b895b833ea15b7fad0e059e56ff3b135d
ariframaps <ariframa@student.ub.ac.id>
2024-04-09 15:23:56 +0700

    setup darkmode feature

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png"
 export const Footer = () => {
     return (
         <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-            <div className="w-full max-w-screen mx-auto p-4 md:py-8">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <Link to="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                         <img src={logo} className="h-8" alt="Flowbite Logo" />
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import { Link, NavLink } from "react-router-dom"
 import logo from "../assets/logo.png"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export const Header = () => {
     const [openMenu, setOpenMenu] = useState(true)
     const activeStyle = 'block py-2 px-3 text-white bg-blue-700 rounded xl:bg-transparent xl:text-blue-700 xl:p-0 xl:dark:text-blue-500 xl:font-bold'
     const inactiveStyle = 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 xl:hover:bg-transparent xl:hover:text-blue-700 xl:p-0 xl:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white xl:dark:hover:bg-transparent dark:border-gray-700'
+    const localStorageTheme = JSON.parse(localStorage.getItem('shoplingDarkMode'))
+    const [darkMode, setDarkMode] = useState(localStorageTheme != null ? localStorageTheme : true)
+
+    useEffect(() => {
+        localStorage.setItem('shoplingDarkMode', JSON.stringify(darkMode));
+
+        if (darkMode) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    }, [darkMode])
 
     return (
         <header>
@@ -16,6 +28,11 @@ export const Header = () => {
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Shopling</span>
                     </Link>
                     <div className="flex xl:order-2 space-x-3 xl:space-x-0 rtl:space-x-reverse">
+                        <button onClick={() => setDarkMode(!darkMode)} data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip" type="button" data-toggle-dark="light" className="flex items-center p-2 mr-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                            <svg aria-hidden="true" data-toggle-icon={darkMode ? 'sun' : 'moon'} className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                {darkMode ? <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fillRule="evenodd" clipRule="evenodd"></path> : <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>}
+                            </svg>
+                        </button>
                         <Link to='/cart' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cart</Link>
                         <button onClick={() => setOpenMenu(!openMenu)} data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg xl:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
                             <span className="sr-only">Open main menu</span>
