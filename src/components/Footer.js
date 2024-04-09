ecdde269d5c6876744b3ad42ed8bbb0a513dbb32
ariframaps <ariframa@student.ub.ac.id>
2024-04-09 15:18:28 +0700

    adding footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png"
+
 export const Footer = () => {
     return (
-        <footer>Footer</footer>
+        <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+            <div className="w-full max-w-screen mx-auto p-4 md:py-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <Link to="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                        <img src={logo} className="h-8" alt="Flowbite Logo" />
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Shopling</span>
+                    </Link>
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            <a href="https://instagram.com" target="_blank" className="hover:underline me-4 md:me-6">Instagram</a>
+                        </li>
+                        <li>
+                            <a href="https://facebook.com" target="_blank" className="hover:underline me-4 md:me-6">Facebook</a>
+                        </li>
+                        <li>
+                            <a href="https://youtube.com" target="_blank" className="hover:underline me-4 md:me-6">Youtube</a>
+                        </li>
+                        <li>
+                            <a href="https://amazon.com" target="_blank" className="hover:underline">Amazon</a>
+                        </li>
+                    </ul>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link to="/" className="hover:underline">Shpling™</Link>. All Rights Reserved.</span>
+            </div>
+        </footer>
     )
 }
